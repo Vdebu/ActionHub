9f4832ff639316432fab57c92645c5ca9c2d0ec6
Vdebu <vdebuzzz@gmail.com>
2025-03-28 14:00:29 +0800

    新增JWT生成机制：通过Go语言的jwt库生成一个带有用户信息和有效期的JWT，使用密钥对Header和Payload进行签名，生成最终的JWT字符串。

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 func (app *application) writeJSON(c *gin.Context, code int, message interface{}) {
@@ -14,4 +16,19 @@ func (app *application) hashPassword(pwd string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), 12)
 	// 没有后续操作可以直接返回
 	return string(hash), err
+}
+
+// 生成JWT令牌
+func (app *application) generateJWT(username string) (string, error) {
+	// 指定迁移方法与声明信息
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		// 声明信息
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 72).Unix(), // 过期时间(根据unix时间进行锚定)
+	})
+	// 获取完整签名的JWT
+	// 填入加密与解密用秘钥
+	signedToken, err := token.SignedString([]byte("secret"))
+	// 方便操作直接加上Bearer前缀
+	return "Bearer " + signedToken, err
 }
--- cmd/web/users.go
+++ cmd/web/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"ActionHub/internal/data"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func (app *application) registerUserHandler(c *gin.Context) {
@@ -21,4 +22,14 @@ func (app *application) registerUserHandler(c *gin.Context) {
 	}
 	// 更新密码
 	input.Password = hashedPwd
+	// 生成JWT令牌
+	token, err := app.generateJWT(input.Username)
+	if err != nil {
+		app.serverErrorResponse(c, err)
+		return
+	}
+	// 流程中未出现问题
+	app.writeJSON(c, http.StatusOK, envelop{
+		"JWT Token": token,
+	})
 }
