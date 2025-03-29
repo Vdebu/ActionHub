7bd46e54eaa8bccbbc10acc1192608cb7a284709
Vdebu <vdebuzzz@gmail.com>
2025-03-29 23:16:08 +0800

    新增配置CORS的中间件：使用gin-cors快速设置允许的源与请求方法等CORS信息。

--- cmd/api/routers.go
+++ cmd/api/routers.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 // 初始化路由
 func (app *application) routers() *gin.Engine {
 	// 使用gin的default(包含logger与recover)
 	r := gin.Default()
+	// 配置CORS跨源请求
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:9393"},                   // 允许的源
+		AllowMethods:     []string{"GET", "POST"},                             // 允许的请求方法
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // 允许的请求头
+		ExposeHeaders:    []string{"Content-Length"},                          // 允许前端访问的响应头
+		AllowCredentials: true,                                                // 允许发送验证凭证(cookie等)
+		MaxAge:           12 * time.Hour,                                      // 预检请求的缓存时间
+		//AllowOriginFunc: func(origin string) bool {}, 						  动态决定允许的源上边定义的切片就会失效
+	}))
 	auth := r.Group("/api/auth")
 	{
 		// 用户登入与注册
